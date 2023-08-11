8960db9dca3c59d9d2556ab92d0465e054b8eefa
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-11 15:09:15 -0300

    🧪 test: create product integration test

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -1,10 +1,26 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import ProductModel from '../../../src/database/models/product.model';
+import productMock from '../../mocks/product.mock';
+import app from '../../../src/app';
 
 chai.use(chaiHttp);
 
 describe('POST /products', function () { 
   beforeEach(function () { sinon.restore(); });
+
+  it('should send status 200 with the new product created', async function(){
+    const mockCreateReturn = ProductModel.build(productMock.validProduct);
+    sinon.stub(ProductModel, 'create').resolves(mockCreateReturn);
+
+    const httpResponse = await chai
+    .request(app)
+    .post('/products')
+    .send(productMock.validProduct)
+
+    expect(httpResponse.status).to.equal(201)
+    expect(httpResponse.body).to.be.deep.equal(mockCreateReturn.dataValues)
+  })
 
 });
---
+++ tests/mocks/product.mock.ts
@@ -0,0 +1,9 @@
+import { Product } from "../../src/types/Product";
+
+const validProduct = {
+  name: "Lança de Athena",
+  orderId: 3,
+  price: "7 peças de ouro",
+};
+
+export default { validProduct }
