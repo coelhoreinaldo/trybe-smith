ea972390606721f47f35392808c38e867bde817d
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-12 09:32:09 -0300

    🧪 test: test list findall orders

--- tests/integration/orders/list.test.ts
+++ tests/integration/orders/list.test.ts
@@ -1,10 +1,20 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import orderMock from '../../mocks/order.mock';
+import OrderModel from '../../../src/database/models/order.model';
+import app from '../../../src/app';
 
 chai.use(chaiHttp);
 
 describe('GET /orders', function () { 
   beforeEach(function () { sinon.restore(); });
+  it('should return 200 and a list of orders', async function () {
+    sinon.stub(OrderModel, 'findAll').resolves(orderMock.allOrders as any);
+    const response = await chai.request(app).get('/orders');
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.be.deep.equal(orderMock.allOrders);
+  }
+  );
 
 });
---
+++ tests/mocks/order.mock.ts
@@ -0,0 +1,27 @@
+const allOrders = [
+  {
+    "id": 1,
+    "userId": 1,
+    "productIds": [
+      2,
+      1
+    ]
+  },
+  {
+    "id": 2,
+    "userId": 3,
+    "productIds": [
+      4,
+      3
+    ]
+  },
+  {
+    "id": 3,
+    "userId": 2,
+    "productIds": [
+      5
+    ]
+  }
+]
+
+export default { allOrders }
